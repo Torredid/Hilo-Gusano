322a4e402051289a36070302deabf515e46f56ef
SerchGGA <sergiogael@gmail.com>
2025-11-19 18:51:10 -0600

    Agrego un hilojardinero
    Vamos a analizar la logica del hilo jardinero

---
+++ HiloJardinero.java
@@ -0,0 +1,54 @@
+public class HiloJardinero extends Thread {
+    private char[][] jardin;
+    
+    public HiloJardinero(char[][] mundo){
+        this.jardin = mundo;
+    }
+    
+
+    private boolean estaSucio(){
+        for(int r=0; r<jardin.length; r++){
+            for(int c=0; c<jardin[0].length; c++){
+                if(jardin[r][c] == 'C' || jardin[r][c] == 'W'){
+                    return true; 
+                }
+            }
+        }
+        return false;
+    }
+    
+    // Metodo para restaurar la tierra
+    private void limpiar(){
+        for(int r=0; r<jardin.length; r++){
+            for(int c=0; c<jardin[0].length; c++){
+                if(jardin[r][c] == 'C' || jardin[r][c] == 'W'){
+                    jardin[r][c] = '*';
+                }
+            }
+        }
+        System.out.println("Jardinero: Limpie el jardin.");
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try{
+                synchronized(jardin){
+                    // Si NO esta sucio, nos esperamos
+                    if(!estaSucio()){
+                        System.out.println("Jardinero: Todo limpio, a dormir (wait).");
+                        jardin.wait();
+                    }
+                    
+                    // Si despertamos o estaba sucio, limpiamos
+                    limpiar();
+                }
+                // Descansamos un poco para no saturar
+                sleep(2000);
+            }
+            catch(InterruptedException e){
+                System.out.println("Interrupcion Jardinero");
+            }
+        }
+    }
+}
